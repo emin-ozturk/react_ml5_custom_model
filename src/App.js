af2098010cad6285f175e36130b40625e9073d5d
Emin Öztürk <eminozturk963@gmail.com>
2024-01-05 18:04:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from "react";
 import Input from './Input';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 function App() {
   const [model, setModel] = useState(null);
@@ -8,6 +9,7 @@ function App() {
   const [label, setLabel] = useState([]);
   const [image, setImage] = useState([]);
   const [info, setInfo] = useState(null);
+  const [loss, setLoss] = useState([]);
 
   const trainModel = () => {
     setInfo("Model Hazırlanıyor...");
@@ -40,6 +42,7 @@ function App() {
         let i = 1;
         customModel.train((lossValue) => {
           if (lossValue) {
+            setLoss(prevLoss => [...prevLoss, { iteration: i, lossValue: lossValue }]);
             console.log('Eğitim kaybı: ', lossValue, i++);
           } else {
             console.log('Eğitim tamamlandı.');
@@ -89,47 +92,50 @@ function App() {
   }
 
   return (
-    <div className='flex flex-row items-center justify-center w-full h-full mx-24 mt-12'>
-      <div className='flex flex-col mr-10'>
+    <div className='flex flex-col items-center justify-center w-full h-full mt-12'>
+      <div className='flex flex-row items-center justify-center flex-wrap'>
         <Input name="0" setLabel={handleLabel} label={label[0]} image={loadImage} />
         <Input name="1" setLabel={handleLabel} label={label[1]} image={loadImage} />
       </div>
 
-      <div className='bg-white drop-shadow-2xl mr-10 min-w-96 rounded-lg h-96 p-5'>
-        <button
-          className='w-full bg-blue-600 text-white font-medium text-sm p-2 rounded'
-          onClick={trainModel}>
-          Modeli Eğit
-        </button>
+      <div className='w-full flex flex-row items-center justify-center px-10 my-16'>
 
-      </div>
-
-      <div className='bg-white drop-shadow-2xl rounded-lg p-5'>
-        <h2 className='text-black font-medium'>Test</h2>
-        <input id="testImage" type="file" onChange={handleClassifyImage}
-          className="block w-full text-sm text-gray-500
-                  file:me-4 file:py-2 file:px-4
-                  file:rounded-lg file:border-0
-                  file:text-sm file:font-semibold
-                  file:bg-blue-600 file:text-white
-                  hover:file:bg-blue-700 mt-3 mb-5 "/>
-
-        <img id='imgTest' className='min-w-64 min-h-64 max-w-64 max-h-64' />
-        {result && (
-          <div>
-            <p>{result[0].label}</p>
-          </div>
-        )}
-      </div>
-
-      {info ?
-        <div className='absolute absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                        bottom-10 bg-blue-500 text-white px-10 py-2 rounded flex justify-center items-center'>
-          {info}
+        <div className='bg-white drop-shadow-2xl mr-10 w-2/3 rounded-lg h-[420px] p-5'>
+          <ResponsiveContainer width="100%" height="80%">
+            <LineChart data={loss} >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="iteration" tick={{ fontSize: 14 }} />
+              <YAxis tick={{ fontSize: 14 }} domain={[-1, 1]} />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="lossValue" stroke="#82ca9d" dot={false} />
+            </LineChart>
+          </ResponsiveContainer>
+          <button
+            className='w-full bg-blue-600 text-white font-medium text-sm p-2 rounded'
+            onClick={trainModel}>
+            Modeli Eğit
+          </button>
+          {info ?
+            <div className='text-blue-500 px-10 py-2 rounded flex justify-center items-center'>
+              {info}
+            </div>
+            : ""
+          }
         </div>
-        : ""
-      }
 
+        <div className='h-[420px] bg-white drop-shadow-2xl rounded-lg p-5'>
+          <h2 className='text-black font-medium'>Test</h2>
+          <input id="testImage" type="file" onChange={handleClassifyImage} className="input" />
+
+          <img id='imgTest' alt="" className='min-w-64 min-h-64 max-w-64 max-h-64' />
+          {result && (
+            <div>
+              <p className='font-medium text-sm mt-3'>{result[0].label}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/Input.js
+++ src/Input.js
@@ -3,10 +3,10 @@ function Input({ name, setLabel, label, image }) {
         const files = e.target.files;
         const validImageFiles = Array.from(files).map((file) => ({ image: file, label }));
         image(validImageFiles);
-      };
+    };
 
     return (
-        <div className='bg-white drop-shadow-2xl w-[600px] h-44 rounded-lg my-5 p-5'>
+        <div className='bg-white drop-shadow-2xl min-w-[500px] h-44 rounded-lg m-5 p-5'>
             <input
                 name={name}
                 type='text'
@@ -14,14 +14,11 @@ function Input({ name, setLabel, label, image }) {
                 className='text-black font-medium'
                 onChange={setLabel} />
 
-            <input type="file" multiple
+            <input
+                type="file"
+                multiple
                 onChange={handleImageChange}
-                className="block w-full text-sm text-gray-500
-                  file:me-4 file:py-2 file:px-4
-                  file:rounded-lg file:border-0
-                  file:text-sm file:font-semibold
-                  file:bg-blue-600 file:text-white
-                  hover:file:bg-blue-700 "/>
+                className="input" />
         </div>
     )
 };
