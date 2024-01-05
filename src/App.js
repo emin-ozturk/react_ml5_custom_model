b34846a2720700af1fbd1da5cc5962483b13956b
Emin Öztürk <eminozturk963@gmail.com>
2024-01-05 14:01:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,72 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Input from './Input';
 
 function App() {
-  const [dogImages, setDogImages] = useState([]);
-  const [catImages, setCatImages] = useState([]);
   const [model, setModel] = useState(null);
   const [result, setResult] = useState(null);
-
+  const [label, setLabel] = useState([]);
+  const [image, setImage] = useState([]);
+  const [info, setInfo] = useState(null);
 
   const trainModel = () => {
-    // Köpek ve kedi resimlerini içeren veri setini oluşturun
-    const data = {
-      labels: ['köpek', 'kedi'],
-      data: [dogImages, catImages],
-    };
-
-    // Transfer Learning için Mobilenet modelini yükleyin
+    setInfo("Model Hazırlanıyor...");
     const featureExtractor = window.ml5.featureExtractor('MobileNet', () => {
       console.log('Feature extractor yüklendi.');
       const customModel = featureExtractor.classification();
 
       let counter = 0;
 
-      data.data[0].forEach((img) => {
+      image.forEach(item => {
+        setInfo("Resimler Yükleniyor...");
         const imageElement = new Image();
-        imageElement.src = URL.createObjectURL(img);
+        imageElement.src = URL.createObjectURL(item.image);
 
-        // Resim yüklendikten sonra callback
         imageElement.onload = () => {
-          customModel.addImage(imageElement, 'köpek', () => {
+          customModel.addImage(imageElement, item.label, () => {
             counter++;
-            console.log('Köpek resmi eklendi.');
+            console.log(item.label + ' resmi eklendi.');
 
-            if (counter === data.data[0].length + data.data[1].length) {
-              // Tüm resimler eklendiyse modeli eğitin
+            if (counter === image.length) {
               trainCustomModel(customModel);
             }
           });
         };
-      });
+      })
 
-      // Cat images
-      data.data[1].forEach((img) => {
-        const imageElement = new Image();
-        imageElement.src = URL.createObjectURL(img);
-
-        // Resim yüklendikten sonra callback
-        imageElement.onload = () => {
-          customModel.addImage(imageElement, 'kedi', () => {
-            counter++;
-            console.log('Kedi resmi eklendi.');
-
-            if (counter === data.data[0].length + data.data[1].length) {
-              // Tüm resimler eklendiyse modeli eğitin
-              trainCustomModel(customModel);
-            }
-          });
-        };
-      });
-
-      // Veri setini kullanarak modeli eğitin
       const trainCustomModel = (customModel) => {
+        setInfo("Model Eğitiliyor...");
         customModel.train((lossValue) => {
           if (lossValue) {
             console.log('Eğitim kaybı: ', lossValue);
           } else {
             console.log('Eğitim tamamlandı.');
+            setInfo("Eğitim Tamamlandı");
+            setInterval(() => {
+              setInfo(null);
+            }, 2000);
+
             setModel(customModel);
           }
         });
@@ -74,19 +53,7 @@ function App() {
     });
   };
 
-  const handleDogImageChange = (e) => {
-    const files = e.target.files;
-    const validImageFiles = Array.from(files).filter((file) => file.type.startsWith('image/'));
-    setDogImages(validImageFiles);
-  };
-
-  const handleCatImageChange = (e) => {
-    const files = e.target.files;
-    const validImageFiles = Array.from(files).filter((file) => file.type.startsWith('image/'));
-    setCatImages(validImageFiles);
-  };
-
-  const classifyImage = () => {
+  const handleClassifyImage = (e) => {
     if (model) {
       const image = document.getElementById('testImage');
       const selectedFile = image.files[0];
@@ -101,8 +68,6 @@ function App() {
               console.error(err);
             } else {
               setResult(results);
-
-              console.log(result)
             }
           });
         }
@@ -112,26 +77,57 @@ function App() {
     }
   };
 
+  const handleLabel = (e) => {
+    setLabel({ ...label, [e.target.name]: e.target.value });
+    console.log(label)
+  }
+
+  const x = (x) => {
+    setImage([...image, ...x]);
+  }
+
   return (
-    <div>
-      <h2>Köpek Resimleri</h2>
-      <input type="file" multiple onChange={handleDogImageChange} />
+    <div className='p-24 flex flex-row items-center h-full'>
+      <div className='flex flex-col mr-10'>
+        <Input name="0" setLabel={handleLabel} label={label[0]} image={x} />
+        <Input name="1" setLabel={handleLabel} label={label[1]} image={x} />
+      </div>
 
-      <h2>Kedi Resimleri</h2>
-      <input type="file" multiple onChange={handleCatImageChange} />
+      <div className='bg-white drop-shadow-2xl mr-10 min-w-64 h-96 p-5'>
+        <button
+          className='w-full bg-blue-600 text-white font-medium text-sm p-2 rounded'
+          onClick={trainModel}>
+          Modeli Eğit
+        </button>
 
-      <button onClick={trainModel}>Modeli Eğit</button>
+      </div>
 
-      <h2>Test İçin Resim Seç</h2>
-      <input id="testImage" type="file" />
-      <button onClick={classifyImage}>Resmi Sınıflandır</button>
+      <div className='bg-white drop-shadow-2xl min-w-64 mr-10 p-5'>
+        <h2 className='text-black font-medium'>Test</h2>
+        <input id="testImage" type="file" onChange={handleClassifyImage}
+          className="block w-full text-sm text-gray-500
+                  file:me-4 file:py-2 file:px-4
+                  file:rounded-lg file:border-0
+                  file:text-sm file:font-semibold
+                  file:bg-blue-600 file:text-white
+                  hover:file:bg-blue-700 "/>
 
-      {result && (
-        <div>
-          <h2>Sınıflandırma Sonuçları:</h2>
-          <p>{result[0].label}</p>
+        {result && (
+          <div>
+            <h2>Sınıflandırma Sonuçları:</h2>
+            <p>{result[0].label}</p>
+          </div>
+        )}
+      </div>
+
+      {info ?
+        <div className='absolute absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                        bottom-10 bg-blue-500 text-white px-10 py-2 rounded flex justify-center items-center'>
+          {info}
         </div>
-      )}
+        : ""
+      }
+
     </div>
   );
 }
